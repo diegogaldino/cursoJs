02f260870c5981dd0c7bd676654a3e996f3f1c07
Diego Galdino <diegogaldino1345@hotmail.com>
2020-02-02 23:55:01 -0300

    iniciando o projeto

---
+++ js/principal.js
@@ -0,0 +1,49 @@
+
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+
+//buscou a primeira tr pelo id
+//var paciente = document.querySelector("#primeiro-paciente");
+
+var pacientes=document.querySelectorAll(".paciente");
+
+for (var i = 0; i <pacientes.length; i++) {
+	var paciente=pacientes[i];
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var tdImc=paciente.querySelector(".info-imc");
+
+	var pesoValido=true;
+	var alturaValida=true;
+
+	if(peso<=0||peso>=1000){
+		console.log("peso invalido");
+		pesoValido=false;
+		tdImc.textContent="peso invalido";
+		//adiciona classe ao elemento
+		paciente.classList.add("paciente-invalido");
+	}
+
+	if(altura <=0 ||altura>=3.00){
+		console.log("altura invalido");
+		alturaValida=false;
+		tdImc.textContent="altura invalido";
+		//paciente.style.backgroundColor="lightcoral";
+		paciente.classList.add("paciente-invalido");
+	}
+
+
+	if (alturaValida&&pesoValido){
+		var imc = peso/(altura*altura);
+		tdImc.textContent=imc.toFixed(2);//toFixed limita casas decimais
+	}
+
+}
+
+
+
